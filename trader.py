92743aaa9557c73b9c07c1d079f8201f845c9185
Petr Matukhov <petr.matukhov@gmail.com>
2018-10-25 14:42:49 +0300

    precalculated weight\win

--- trader.py
+++ trader.py
@@ -1,7 +1,9 @@
 import argparse 
 
-parser = argparse.ArgumentParser(description='Calculate optimal trader strategy')
-parser.add_argument('-f', '--file', type=str, help="File with source data")
+LOT_NOMINAL = 1000 # номинал каждой облигации
+LOT_DAYS = 30 # дней до погашения
+LOT_COUPON_DAYLY = 1 # купонный доход в день
+
 
 
 class TraderException(Exception):
@@ -9,20 +11,36 @@ class TraderException(Exception):
     pass
 
 def parse_input_line(line):
+    """
+    Разбирает строку входных данных и считает выигрыш
+    :param line:
+    :return:
+    """
+
     parts = line.split()
 
-    if not parts == 3:
+    if not len(parts) == 4:
         raise TraderException("Incorrect format '{}'".format(line))
 
     try:
         data = dict(
             day=int(parts[0]),
             name=parts[1],
-            price=float(parts[2]),
+            price_percent=float(parts[2]),
             quantity=int(parts[3]),
         )
     except ValueError as e:
         raise TraderException("Unable to parse '{}'".format(line))
+
+    # стоимость всего лота в деньгах
+    data['lot_price'] = LOT_NOMINAL * \
+                        data['price_percent']/100 * \
+                        data['quantity']
+
+    # выигрышь лота (в дегьшах) = купонный доход + сумма погашения
+    data['lot_win'] = (LOT_NOMINAL +
+                       LOT_DAYS*LOT_COUPON_DAYLY) * \
+                      data['quantity']
 
     return data
 
@@ -34,18 +52,13 @@ def get_data_from_file(filename):
         with open(filename, 'r') as file:
             line_num = 0
             days, lot_count, balance = file.readline().split()
+
             for c in range(0, int(lot_count)):
                 line_num += 1
                 try:
-                    line_parts = file.readline().split()
-                    lot_data = dict(
-                        day=int(line_parts[0]),
-                        name=line_parts[1],
-                        price=float(line_parts[2]),
-                        quantity=int(line_parts[3]),
-                        )
+                    lot_data = parse_input_line(file.readline())
                 except TraderException as e:
-                    print("Error in input data, line {} ".format(line_data, e.message))
+                    print("Error in input data, line {}: {}".format(line_num, e))
 
                 lots.append(lot_data)
     except Exception as e:
@@ -64,9 +77,14 @@ def calculate_optimal_lots(days, lots, balance):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Calculate optimal trader strategy')
+    parser.add_argument('-f', '--file', type=str, help="File with source data")
+
     args = parser.parse_args()
     
     filename = args.file
+
 
     try:
         if filename:
@@ -80,5 +98,5 @@ if __name__ == '__main__':
 
 
     total_win, lot_purchased = calculate_optimal_lots(days, lots, balance)
-    print()
+    # /print()
 
